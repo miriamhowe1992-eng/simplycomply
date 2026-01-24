795b1792e26991dcd5868481f2f028df3b36513a
miriamhowe1992-eng <miriamhowe1992@gmail.com>
2026-01-24 14:03:06 +0000

    Update MongoDB connection URI environment variable

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -3,7 +3,7 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = os.environ["MONGO_URI"]
+MONGO_URI = os.environ["MONGO_URL"]
 
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client.get_default_database()
